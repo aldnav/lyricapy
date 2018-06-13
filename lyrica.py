d5d90b2bcc1a9a7ca654620a99a97db87e4a9b2e
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-06-13 23:51:52 +0800

    Fix pep8 and use docopt

--- lyrica.py
+++ lyrica.py
@@ -1,6 +1,11 @@
+"""
+Usage: lyrica.py <title> <artist>
+"""
+
 import re
 import requests
 from bs4 import BeautifulSoup
+from docopt import docopt
 
 headers = {
     'User-Agent': 'Mozilla/5.0'
@@ -15,12 +20,14 @@ def parse_lyrics(text):
                 'RentAnAdviser.com' in line):
             continue
         ts = line.split(']')[0][1:]
-        l = line.split(']')[1]
-        lyrics_obj.append((ts, l))
+        li = line.split(']')[1]
+        lyrics_obj.append((ts, li))
     return lyrics_obj
 
 
 def get_lyrics(title="", artist=""):
+    title = str(title)
+    artist = str(artist)
     """Get lyrics for a given song via rentanadviser.com"""
     url = 'http://www.rentanadviser.com/en/subtitles/getsubtitle.aspx?'
     r = requests.get(url, params=dict(song=title, artist=artist))
@@ -29,26 +36,34 @@ def get_lyrics(title="", artist=""):
     if 'No subtitle were found!' in r.text:
         return 'Sorry. No lyrics found.'
     soup = BeautifulSoup(r.text, 'lxml')
-    
+
     # request the simple one
     data = {
-        '__EVENTTARGET' : 'ctl00$ContentPlaceHolder1$btnlyricssimple',
-        '__EVENTARGUMENT' : '',
-        'ctl00$txtSearch' : 'Search Apps...',
-        'ctl00$Overcome_Enter_problem_in_IE1' : '',
-        'ctl00$ContentPlaceHolder1$txtsearchsubtitle' : 'Search Subtitle...',
-        'ctl00$ContentPlaceHolder1$Overcome_Enter_problem_in_IE2' : '',
+        '__EVENTTARGET': 'ctl00$ContentPlaceHolder1$btnlyricssimple',
+        '__EVENTARGUMENT': '',
+        'ctl00$txtSearch': 'Search Apps...',
+        'ctl00$Overcome_Enter_problem_in_IE1': '',
+        'ctl00$ContentPlaceHolder1$txtsearchsubtitle': 'Search Subtitle...',
+        'ctl00$ContentPlaceHolder1$Overcome_Enter_problem_in_IE2': '',
     }
     for inp in soup.find_all(id=re.compile('__.*')):
         data[inp.get('name')] = inp.get('value')
 
     r = requests.post(r.url, data=data)
-    lyrics = r.text
+    lyrics = r.text if not r.text.startswith('<html') else ''
+    if lyrics == '':
+        with open('out.html', 'w') as f:
+            f.write(r.text)
     return lyrics
 
 
 if __name__ == '__main__':
-    lyrics = get_lyrics(title='Wiggle', artist='Jason Derulo')
+    args = docopt(__doc__)
+    if not args:
+        import sys
+        sys.exit(0)
+    title = args['<title>']
+    artist = args['<artist>']
+    lyrics = get_lyrics(title=title, artist=artist)
     parsed = parse_lyrics(lyrics)
     print(parsed)
-
