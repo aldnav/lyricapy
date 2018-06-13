2fee059704be04b98e1db0aaf2e6738fdb102abc
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-06-13 17:39:34 +0800

    Get and parse simple lyrics srt

--- lyrica.py
+++ lyrica.py
@@ -1,5 +1,23 @@
+import re
 import requests
 from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0'
+}
+
+
+def parse_lyrics(text):
+    lyrics_obj = []
+    for line in text.split('\n'):
+        line = line.strip()
+        if (len(line) == 0 or
+                'RentAnAdviser.com' in line):
+            continue
+        ts = line.split(']')[0][1:]
+        l = line.split(']')[1]
+        lyrics_obj.append((ts, l))
+    return lyrics_obj
 
 
 def get_lyrics(title="", artist=""):
@@ -11,9 +29,26 @@ def get_lyrics(title="", artist=""):
     if 'No subtitle were found!' in r.text:
         return 'Sorry. No lyrics found.'
     soup = BeautifulSoup(r.text, 'lxml')
-    lyrics_div = soup.find(id='myTabContent')
-    print(list(lyrics_div.children)[1])
+    
+    # request the simple one
+    data = {
+        '__EVENTTARGET' : 'ctl00$ContentPlaceHolder1$btnlyricssimple',
+        '__EVENTARGUMENT' : '',
+        'ctl00$txtSearch' : 'Search Apps...',
+        'ctl00$Overcome_Enter_problem_in_IE1' : '',
+        'ctl00$ContentPlaceHolder1$txtsearchsubtitle' : 'Search Subtitle...',
+        'ctl00$ContentPlaceHolder1$Overcome_Enter_problem_in_IE2' : '',
+    }
+    for inp in soup.find_all(id=re.compile('__.*')):
+        data[inp.get('name')] = inp.get('value')
+
+    r = requests.post(r.url, data=data)
+    lyrics = r.text
+    return lyrics
+
 
 if __name__ == '__main__':
-    get_lyrics(title='Wiggle', artist='Jason Derulo')
+    lyrics = get_lyrics(title='Wiggle', artist='Jason Derulo')
+    parsed = parse_lyrics(lyrics)
+    print(parsed)
 
