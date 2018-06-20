32140a0b5ff8e418368e54cf42309e503cb6b57d
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-06-20 20:30:16 +0800

    Return lyrics as lines

--- lyricactl.py
+++ lyricactl.py
@@ -11,12 +11,16 @@ def clean_title(title):
     return title.strip()
 
 
+def get_lines(lyrics):
+    return '\n'.join([x[1] for x in lyrics])
+
+
 def lyricactl():
     title = subprocess.check_output(get_title_cmd.split())
     artist = subprocess.check_output(get_artist_cmd.split())
     title = clean_title(title.decode())
     print('{} - {}'.format(title, artist))
-    lyrics = parse_lyrics(get_lyrics(title, artist))
+    lyrics = get_lines(parse_lyrics(get_lyrics(title, artist)))
     if len(lyrics) == 0:
         print(':( Lyrics not available.')
     else:
