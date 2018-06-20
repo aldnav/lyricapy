fb59a1b3c70a2c3c56656271021a4c6b08894bf0
Aldrin Navarro <aldrinnavarro16@gmail.com>
2018-06-20 20:58:33 +0800

    Listen mode

--- lyricactl.py
+++ lyricactl.py
@@ -1,8 +1,17 @@
+"""
+Usage: lyricactl.py [<persist>]
+"""
+
 import subprocess
+import time
+from docopt import docopt
 from lyrica import get_lyrics, parse_lyrics
 
 get_title_cmd = 'playerctl metadata title'
 get_artist_cmd = 'playerctl metadata artist'
+get_player_status = 'playerctl status'
+song = ''
+song_d = ''
 
 
 def clean_title(title):
@@ -16,15 +25,37 @@ def get_lines(lyrics):
 
 
 def lyricactl():
-    title = subprocess.check_output(get_title_cmd.split())
-    artist = subprocess.check_output(get_artist_cmd.split())
-    title = clean_title(title.decode())
-    print('{} - {}'.format(title, artist))
+    global song_d
+    global song
+    song_d = song
+    song, title, artist = get_song()
+    print(song)
     lyrics = get_lines(parse_lyrics(get_lyrics(title, artist)))
     if len(lyrics) == 0:
         print(':( Lyrics not available.')
+        song = ''
     else:
         print(lyrics)
 
 
-lyricactl()
+def get_song():
+    title = subprocess.check_output(get_title_cmd.split())
+    artist = subprocess.check_output(get_artist_cmd.split())
+    title = clean_title(title.decode())
+    return ('{} - {}'.format(title, artist), title, artist)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    if not args:
+        import sys
+        sys.exit(0)
+    persist = args['<persist>'] == '1'
+    if persist:
+        while True:
+            song, _, _ = get_song()
+            if song_d != song:
+                lyricactl()
+            time.sleep(0.5)
+    else:
+        lyricactl()
